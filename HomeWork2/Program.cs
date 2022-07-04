7451f7d7127ed54ab1137ac7b24715051f6d8538
LeaClover <plesen_14@mail.ru>
2022-07-04 04:01:54 +0300

    add project HWtask2

---
+++ HomeWork2/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа 
+// или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = Convert.ToInt32(Console.ReadLine());
+            string str = Convert.ToString(number);
+            if (str.Length < 3)
+            {
+                Console.WriteLine("Третьей цифры нет!");
+            }
+            else
+            {
+                Console.WriteLine(str[2]);
+            }
+        }
+       
+    }
+}
