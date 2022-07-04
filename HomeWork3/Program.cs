0d5ee35000b32b81625f1052df399dd3a412482a
LeaClover <plesen_14@mail.ru>
2022-07-04 04:02:03 +0300

    add project HWtask3

---
+++ HomeWork3/Program.cs
@@ -0,0 +1,36 @@
+﻿// Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] str = new string[] {"1", "2", "3", "4", "5", "6", "7"};
+            int number = Convert.ToInt32(Console.ReadLine());
+            string num = Convert.ToString(number);
+            if (number < str.Length+1)
+            {
+                if (num == str[5] ^ num == str[6])
+                    {
+                      Console.WriteLine("Ура! Это выходной!");
+                    }
+                else
+                    {
+                      Console.WriteLine("Не выходной :с");
+                    }
+            }
+            else
+            {
+                Console.WriteLine("Неверное значение!!!");
+            }
+        
+        }
+       
+    }
+}
