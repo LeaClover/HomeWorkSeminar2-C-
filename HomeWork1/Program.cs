ad419df7ba9458779e8eb8c3292e2ab5052e6491
LeaClover <plesen_14@mail.ru>
2022-07-04 04:01:44 +0300

    add project HWtask1

---
+++ HomeWork1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число 
+//и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = Convert.ToInt32(Console.ReadLine());
+            string str = Convert.ToString(number);
+            if (str.Length == 3)
+            {
+                Console.WriteLine(str[1]);
+            }
+            else
+            {
+                Console.WriteLine("НЕВЕРНО");
+            }
+        }
+       
+    }
+}
