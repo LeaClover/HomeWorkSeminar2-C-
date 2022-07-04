a007bbc65aa2ed4ffa63ed5a6759a517ba62c4b2
LeaClover <plesen_14@mail.ru>
2022-07-04 04:01:13 +0300

    add project task1

---
+++ Task1/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая выводит случайное трёхзначное число 
+//и удаляет вторую цифру этого числа. 
+ 
+// 456 -> 46 
+ 
+// 782 -> 72 
+ 
+// 918 -> 98 
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Convert.ToString(GetRandomNumber());
+            Console.WriteLine(str);
+            Console.WriteLine($"{str[0]}{str[2]}");
+        }
+        public static int GetRandomNumber()
+        {
+            int number = new Random().Next(100, 1000);
+            return number;
+        }
+    }
+}
